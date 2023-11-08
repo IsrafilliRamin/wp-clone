14357458575aac7f358ce49a92b7d34af7ff775e
Ramin <raminisrafilli@gmail.com>
2023-11-08 12:01:49 +0400

    feat: scroll height

--- chat.js
+++ chat.js
@@ -1,11 +1,20 @@
 let iconChat = document.querySelector(".fa-user-group");
 let leftMenu = document.querySelector("#left");
 let closeMenu = document.querySelector(".fa-left-long");
+var chatContainer = document.querySelector(".allMessage");
+var chatContainerDivs = [...document.querySelectorAll(".allMessage > div")];
+let allDivsHeight = 0;
+chatContainerDivs.forEach((div) => {
+    allDivsHeight += div.clientHeight
 
-
-iconChat.addEventListener("click",()=>{
+})
+if ((chatContainer.clientHeight - 200) < allDivsHeight) {
+    chatContainer.style.justifyContent = "initial"
+}
+chatContainer.scrollTop = chatContainer.scrollHeight;
+iconChat.addEventListener("click", () => {
     leftMenu.style.left = "0%"
 })
-closeMenu.addEventListener("click",()=>{
+closeMenu.addEventListener("click", () => {
     leftMenu.style.left = "-100%"
 })
